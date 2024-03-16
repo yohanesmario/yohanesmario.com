3183629db4bbe1f99cbc846048b48c627371970d
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2024-03-17 00:15:46 +0700

    remove doctor.sh

--- doctor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Dependencies:"
-which code | sed 's/.*/\- Code : &/'
-which hugo | sed 's/.*/\- Hugo : &/'
-which git  | sed 's/.*/\- Git  : &/'
-which read | sed 's/.*/\- Read : &/'
